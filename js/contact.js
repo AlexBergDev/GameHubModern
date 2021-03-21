8778e1b6c0ce2e40a6fe627db53d08c7a8e8bc12
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-03-21 16:23:11 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,50 @@
-const form = document.querySelector("form");
+const form = document.querySelector("#contactForm");
 const name = document.querySelector("#name");
-const address = document.querySelector("#address");
+const nameError = document.querySelector("#nameError");
 const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
 const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
 const success = document.querySelector("#success");
-const button = document.querySelector("button");
+const button = document.querySelector("#contactButton");
+
+function validateForm(event) {
+    event.preventDefault();
+
+    if (checkLength(name.value, 1) === true) {
+        nameError.style.display = "none";
+    } else {
+        nameError.style.display = "block";
+    }
+
+    if (checkLength(subject.value, 10) === true) {
+        subjectError.style.display = "none";
+    } else {
+        subjectError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+}
+
+form.addEventListener("submit", validateForm);
 
 function checkIfButtonIsDisabled() {
-    if (checkLength(name.value, 1) && checkLength(address.value, 25) && validateEmail(email.value) && checkLength(subject.value, 10)) {
+
+    if (checkLength(name.value, 1) && checkLength(subject.value, 10) && validateEmail(email.value)) {
         button.disabled = false;
     } else {
-        success.innerHTML = "";
+
+        message.innerHTML = "";
+
         button.disabled = true;
     }
 }
 
 name.addEventListener("keyup", checkIfButtonIsDisabled);
-address.addEventListener("keyup", checkIfButtonIsDisabled);
 email.addEventListener("keyup", checkIfButtonIsDisabled);
 subject.addEventListener("keyup", checkIfButtonIsDisabled);
 
@@ -27,3 +55,17 @@ function submitForm(event) {
 }
 
 form.addEventListener("submit", submitForm);
+
+function checkLength(value, len) {
+    if (value.trim().length >= len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
